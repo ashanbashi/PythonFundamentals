5899049aae67b9ce3acdca5a4167d4d756d81faa
ashanbashi <ashanbashi23@gmail.com>
2026-02-10 16:53:58 +0300

    iniial commit

---
+++ DataTypes.py
@@ -0,0 +1,28 @@
+age = 70 #interger
+temperature = 34.89 #float
+greeting =  "Hello" #string
+isMaried = True #boolean
+
+print("I am", age, "years old")
+print("The current body temperature of the patient is", temperature, "degrees Celsius")
+print("Are you maried ?", isMaried)
+
+
+#Data Structures - Multiple values stored in a single variable
+cars = ["Toyota", "Audi", "BMW", "Mercedes"] #List- it's ordered and changeable
+fruits = ("apple", "banana", "cherry") #Tuple - it's ordered and unchangeable
+countries = {"Italy", "Germany", "France"} #Set - it's unordered and unchangeable
+student = {
+    "firstname" : "John",
+    "lastname" : "Doe",
+    "age" : 19,
+    "course" : "FullStack",
+    "gender" : "Male",
+}
+print(cars)
+print(fruits)
+print(countries)
+print(student)
+print(student["firstname"])
+
+#Ttpecasting
---
+++ variables.py
@@ -0,0 +1,17 @@
+#Variable-container used to store a value
+
+firstname = "Musab"
+school = "eMobilis"
+lastname = "Abass"
+num1 = 56
+num2 = 20
+language = "Python"
+
+
+print(firstname)
+print(school)
+print(lastname)
+print(firstname,lastname)
+print(school,"is a coding school")
+print(num1+num2)
+print(language, "is a web programming language")
