8bc957bec04664c13c36094aeabb1c0156d46a82
ashanbashi <ashanbashi23@gmail.com>
2026-02-19 14:37:58 +0300

    initial commit

--- encapsulation.py
+++ encapsulation.py
@@ -1,1 +1,16 @@
 #Hiding data so that other classes do not have access to it
+
+class BankAccount:
+    def __init__(self, balance):
+        self.__balance = balance   # private variable
+
+    def deposit(self, amount):
+        self.__balance += amount
+
+    def get_balance(self):
+        return self.__balance
+
+account = BankAccount(2000)
+account.deposit(600)
+print(account.get_balance())
+
